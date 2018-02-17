250eead62d488065e53007bf86f30c9cdba250aa
Sydney Turnbull <turnbull.sydney@gmail.com>
2018-02-17 15:10:28 -0500

    Add project files.

---
+++ MijemApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MijemApplication.Startup))]
+namespace MijemApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
